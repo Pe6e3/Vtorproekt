9e0ea58870f13a7593bc0febe5e6a4896e59b587
Pe6e3 <xpe6e3@gmail.com>
2023-03-31 17:46:28 +0600

    Added icons to each view (add/edit/inf/delete) some design in tables, translation of text

--- Vtorproekt_v1/Models/Storage.cs
+++ Vtorproekt_v1/Models/Storage.cs
@@ -7,6 +7,7 @@ public class Storage
     [Display (Name ="Склад")]
     [Key] public int StorageId { get; set; }
 
+    [Display(Name ="Адрес склада")]
     [StringLength(40, MinimumLength = 2, ErrorMessage = "Длина строки должна быть от 2 до 40 символов")]
     public string StorageAdress { get; set; } = string.Empty;
 }
