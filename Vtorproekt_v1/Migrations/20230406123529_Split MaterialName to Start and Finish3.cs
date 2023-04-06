4ecb09e31f1ff65c2eb0d63f57dcf4246458eb78
Pe6e3 <xpe6e3@gmail.com>
2023-04-06 18:39:44 +0600

    Сделал миграцию, обновил БД

---
+++ Vtorproekt_v1/Migrations/20230406123529_Split MaterialName to Start and Finish3.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vtorproekt.Migrations
+{
+    /// <inheritdoc />
+    public partial class SplitMaterialNametoStartandFinish3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "MaterialName",
+                table: "Materials",
+                newName: "MaterialNameStart");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "MaterialNameStart",
+                table: "Materials",
+                newName: "MaterialName");
+        }
+    }
+}
